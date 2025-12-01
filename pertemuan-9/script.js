7dc51875f0812448c7a59aed14198820ea805055
RizkyJulian <muhamadrizkyjulian319@gmail.com>
2025-12-01 16:25:55 +0700

    Update Commit

---
+++ pertemuan-9/script.js
@@ -0,0 +1,7 @@
+function tambahBorder() {
+    document.getElementById("box").style.border = "3px solid blue";
+}
+
+function hapusBorder() {
+    document.getElementById("box").style.border = "none";
+}
